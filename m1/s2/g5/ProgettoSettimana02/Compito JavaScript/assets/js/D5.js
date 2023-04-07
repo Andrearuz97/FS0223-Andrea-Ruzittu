a524227f18b6fd0e2b0757ff2d127270feeb0546
Andrearuz97 <ruzittu.andrea97@gmail.com>
2023-04-07 23:25:27 +0200

    intervento sui  console.log

--- m1/s2/g5/ProgettoSettimana02/Compito JavaScript/assets/js/D5.js
+++ m1/s2/g5/ProgettoSettimana02/Compito JavaScript/assets/js/D5.js
@@ -12,24 +12,22 @@ REGOLE
 const pets = ['dog', 'cat', 'hamster', 'redfish']
 
 pets.forEach(pet => {
-return (pet);
+console.log(pet);
 });
-console.log(pets);
 
 /* ESERCIZIO 2
     Scrivi del codice per ordinare alfabeticamente gli elementi dell'array "pets".
 */
 pets.sort().forEach(pet => {
-return (pet);
+console.log(pet);
 });
-console.log(pets);
 /* ESERCIZIO 3
     Scrivi del codice per stampare nuovamente in console gli elementi dell'array "pets", questa volta in ordine invertito.
 */
 pets.reverse().forEach(pet => {
-    return (pet);
+    console.log(pet);
+   
 })
-console.log(pets);
 /* ESERCIZIO 4
     Scrivi del codice per spostare il primo elemento dall'array "pets" in ultima posizione.
 */
@@ -64,9 +62,9 @@ const cars = [
 ]
     cars.forEach((car, index) => {
         car.licensePlate = `AB-${index + 100}-CD`;
+        console.log(car);
     });
 
-    console.log(cars);
 /* ESERCIZIO 6
     Scrivi del codice per aggiungere un nuovo oggetto in ultima posizione nell'array "cars", rispettando la struttura degli altri elementi.
     Successivamente, rimuovi l'ultimo elemento della proprietà "trims" da ogni auto.
@@ -83,9 +81,9 @@ cars.push(newCar);
 for (let i = 0; i < cars.length; i++) {
     const car = cars[i];
     car.trims.pop();
+    console.log(car);
 }
 
-console.log(cars);
 /* ESERCIZIO 7
     Scrivi del codice per salvare il primo elemento della proprietà "trims" di ogni auto nel nuovo array "justTrims", sotto definito.
 */
@@ -94,8 +92,8 @@ for (let i = 0; i < cars.length; i++) {
     const car = cars[i];
     justTrims.push(car.trims[0]);
 }
-
 console.log(justTrims);
+
 
 /* ESERCIZIO 8
     Cicla l'array "cars" e costruisci un if/else statament per mostrare due diversi messaggi in console. Se la prima lettera della proprietà
