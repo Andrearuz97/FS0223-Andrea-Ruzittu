71fa2e2052bf0b9b33ea1bef26822847ba73ceb7
Andrearuz97 <ruzittu.andrea97@gmail.com>
2023-04-12 17:16:44 +0200

    esercitazione D7.js

---
+++ m1/s3/g2/lezione/js/script.js
@@ -0,0 +1,12 @@
+let array = ["ciao", "come", "stai","ccici"]
+console.log(array.reduce());
+
+function somma (... numeri){
+let risultato = 0
+for (let numero of numeri){
+risultato += numero
+}}
+console.log(somma(2,3));
+
+var primoArray = ["Giorgio", "Mario", "Penelope"];
+var secondoArray = ["Mariangela", "Flavio", "Polletto"];
