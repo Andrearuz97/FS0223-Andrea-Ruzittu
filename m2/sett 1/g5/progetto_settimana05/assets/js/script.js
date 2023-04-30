86d886529b098df1d280526c89101a6f3fd83a1d
Andrearuz97 <ruzittu.andrea97@gmail.com>
2023-04-30 14:24:22 +0200

    formattazione

--- m2/sett 1/g5/progetto_settimana05/assets/js/script.js
+++ m2/sett 1/g5/progetto_settimana05/assets/js/script.js
@@ -1,42 +1,31 @@
 let scrollTopBar = 450;
 
 window.onscroll = function () {
-
-    if (window.scrollY > scrollTopBar || window.pageYOffset > scrollTopBar) {
-        document.querySelector('nav').classList.add('white');
-        document.querySelector('.nav-btn').classList.add('green');
-    }
-
-    else {
-        document.querySelector('nav').classList.remove('white');
-        document.querySelector('.nav-btn').classList.remove('green');
-    }
+  if (window.scrollY > scrollTopBar || window.pageYOffset > scrollTopBar) {
+    document.querySelector("nav").classList.add("white");
+    document.querySelector(".nav-btn").classList.add("green");
+  } else {
+    document.querySelector("nav").classList.remove("white");
+    document.querySelector(".nav-btn").classList.remove("green");
+  }
 };
 
-
-
 let m = document.querySelectorAll("svg path");
-let keyframes = [  { opacity: 0, offset: 0 },  { opacity: 0, offset: 0 },  { opacity: 0, offset: 1 }];
+let keyframes = [
+  { opacity: 0, offset: 0 },
+  { opacity: 0, offset: 0 },
+  { opacity: 0, offset: 1 },
+];
 
 function animate() {
   let random = Math.floor(Math.random() * m.length);
   let mSelected = m[random];
-  
+
   if (mSelected) {
     mSelected.animate(keyframes, { duration: 2000 });
   }
-  
+
   setTimeout(animate, 100);
 }
 
 animate();
-
-
-
-
-
-
-
-
-
-
