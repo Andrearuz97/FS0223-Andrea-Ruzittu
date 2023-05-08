a0eb0a21a7817e42cd15576264be393e9553c7b5
Andrearuz97 <ruzittu.andrea97@gmail.com>
2023-05-08 16:02:10 +0200

    Esercizio js 3-modulo

---
+++ m2/sett 3/g1/Compito/assets/js/form.js
@@ -0,0 +1,49 @@
+class Pet {
+    constructor(petName, ownerName, species, breed) {
+      this.petName = petName;
+      this.ownerName = ownerName;
+      this.species = species;
+      this.breed = breed;
+    }
+  
+    hasSameOwner(otherPet) {
+      return this.ownerName === otherPet.ownerName;
+    }
+  }
+  
+  const form = document.querySelector('#pet-form');
+  const petList = document.querySelector('#pet-list');
+  
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+  
+    const petName = form.elements['pet-name'].value;
+    const ownerName = form.elements['owner-name'].value;
+    const species = form.elements['species'].value;
+    const breed = form.elements['breed'].value;
+  
+    const pet = new Pet(petName, ownerName, species, breed);
+  
+    const listItem = document.createElement('li');
+    listItem.textContent = `Il tuo animaletto si chiama ${pet.petName} è un/una (${pet.species}, di razza  ${pet.breed}). Il proprietario si chiama: ${pet.ownerName}`;
+  
+    // Confronto il proprietario di ogni nuovo animale con quelli già presenti nella lista
+    const petsInList = petList.querySelectorAll('li');
+    for (let i = 0; i < petsInList.length; i++) {
+      const otherPet = petsInList[i].pet;
+      if (pet.hasSameOwner(otherPet)) {
+        const message = `${pet.petName} e ${otherPet.petName} hanno lo stesso proprietario!`;
+        const alert = document.createElement('div');
+        alert.classList.add('alert');
+        alert.textContent = message;
+        form.appendChild(alert);
+        setTimeout(() => alert.remove(), 3000);
+        break;
+      }
+    }
+  
+    listItem.pet = pet;
+    petList.appendChild(listItem);
+    form.reset();
+  });
+  
--- m2/sett 3/g1/Compito/assets/js/script.js
+++ m2/sett 3/g1/Compito/assets/js/script.js
@@ -1,25 +1,24 @@
 class User {
-    constructor(firstName, lastName, age, location) {
-      this.firstName = firstName;
-      this.lastName = lastName;
-      this.age = age;
-      this.location = location;
-    }
-  
-    compareAge(otherUser) {
-      if (this.age > otherUser.age) {
-        return `${this.firstName} ${this.lastName} ha ${this.age} anni ed è più grande di ${otherUser.firstName} ${otherUser.lastName} che ne ha ${otherUser.age}`;
-      } else if (this.age < otherUser.age) {
-        return `${this.firstName} ${this.lastName} ha ${this.age} anni è più giovane di ${otherUser.firstName} ${otherUser.lastName} che ne ha ${otherUser.age}`;
-      } else {
-        return `${this.firstName} ${this.lastName} ha la stessa età di ${otherUser.firstName} ${otherUser.lastName}, ovvero ${this.age} anni.`;
-      }
+  constructor(firstName, lastName, age, location) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.location = location;
+  }
+
+  compareAge(otherUser) {
+    if (this.age > otherUser.age) {
+      return `${this.firstName} ${this.lastName} ha ${this.age} anni ed è più grande di ${otherUser.firstName} ${otherUser.lastName} che ne ha ${otherUser.age}`;
+    } else if (this.age < otherUser.age) {
+      return `${this.firstName} ${this.lastName} ha ${this.age} anni è più giovane di ${otherUser.firstName} ${otherUser.lastName} che ne ha ${otherUser.age}`;
+    } else {
+      return `${this.firstName} ${this.lastName} ha la stessa età di ${otherUser.firstName} ${otherUser.lastName}, ovvero ${this.age} anni.`;
     }
   }
-  
-  const user1 = new User("Antonio", "cassano", 40, "Bari");
-  const user2 = new User("Francesco", "Totti", 37, "Roma");
-  const user3 = new User("Antonio", "Di natale",40, "Udine");
-  
-  console.log(user1.compareAge(user3)); // Output: "Mario è più grande di Luigi"
-  
+}
+
+const user1 = new User("Antonio", "cassano", 40, "Bari");
+const user2 = new User("Francesco", "Totti", 37, "Roma");
+const user3 = new User("Antonio", "Di natale", 40, "Udine");
+
+console.log(user1.compareAge(user3)); // Output: "Mario è più grande di Luigi"
