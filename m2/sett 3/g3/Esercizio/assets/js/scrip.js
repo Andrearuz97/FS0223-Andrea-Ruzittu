0a3275d5180ab78412386d71cea3c3d21d70d44d
Andrearuz97 <ruzittu.andrea97@gmail.com>
2023-05-10 17:55:41 +0200

    update

--- m2/sett 3/g3/Esercizio/assets/js/scrip.js
+++ m2/sett 3/g3/Esercizio/assets/js/scrip.js
@@ -1,51 +1,34 @@
-// Chiamata HTTP di tipo GET
 fetch("https://striveschool-api.herokuapp.com/books")
   .then((response) => response.json())
   .then((data) => {
     const libraryElement = document.getElementById("library");
+    let cardsHTML = '';
+
     data.forEach((book) => {
-      // Creiamo una card per ogni libro
-      const card = document.createElement("div");
-      card.classList.add("card", "mb-3");
+      // Genera il codice HTML per la singola carta del libro
+      const cardHTML = `
+        <div class="card mb-3">
+          <img src="${book.img}" class="card-img-top" alt="Book Image">
+          <div class="card-body">
+            <p class="card-text">Tipo: ${book.category}</p>
+            <p class="card-text">Prezzo: ${book.price}</p>
+            <button class="btn btn-danger" onclick="removeCard(this)">Scarta</button>
+          </div>
+        </div>
+      `;
 
-      // inseriamo l'immagine del libro nella card
-      const image = document.createElement("img");
-      image.src = book.img;
-      image.classList.add("card-img-top");
-      card.appendChild(image);
-
-      // creiamo ilcorpo della card
-      const cardBody = document.createElement("div");
-      cardBody.classList.add("card-body");
-
-      // Inserimento del tipo del libro nel corpo della card
-      const type = document.createElement("p");
-      type.classList.add("card-text");
-      type.innerHTML = `Tipo: ${book.category}`;
-      cardBody.appendChild(type);
-
-      // Inserimento del prezzo del libro nel corpo della card
-      const price = document.createElement("p");
-      price.classList.add("card-text");
-      price.innerHTML = `Prezzo: ${book.price}`;
-      cardBody.appendChild(price);
-
-      //pulsante per rimuovere il libro
-      const removeButton = document.createElement("button");
-      removeButton.classList.add("btn", "btn-danger");
-      removeButton.innerHTML = "Scarta";
-      removeButton.addEventListener("click", () => {
-        card.remove(); // Rimozione della card quando il pulsante viene premuto
-      });
-      cardBody.appendChild(removeButton);
-
-      // Inserimento del corpo della card nella card stessa
-      card.appendChild(cardBody);
-
-      // Aggiunta card alla libreria
-      libraryElement.appendChild(card);
+      cardsHTML += cardHTML;
     });
+
+    // Aggiunge il codice HTML delle carte alla libreria
+    libraryElement.innerHTML = cardsHTML;
   })
   .catch((error) => {
     console.error("Si è verificato un errore:", error);
   });
+
+// Funzione per rimuovere la carta quando il pulsante "Scarta" viene premuto
+function removeCard(button) {
+  const card = button.closest(".card");
+  card.remove();
+}
