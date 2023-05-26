62bb0038f919964f2a9ce5ba02fb8298e577ddde
Andrea Ruzittu <ruzittu.andrea97@gmail.com>
2023-05-26 12:58:52 +0200

    progettoSettimana09

--- m3/s1/g5/progettoSettimana09/assets/js/progetto.js
+++ m3/s1/g5/progettoSettimana09/assets/js/progetto.js
@@ -7,7 +7,7 @@ class FirstUser {
         this.carica += unaRicarica;
     }
     chiamata(minutiDurata) {
-        const costoMinuto = 0.20; //20 cent è il costo al minuto della chiamata.
+        const costoMinuto = 0.2; //20 cent è il costo al minuto della chiamata.
         const costoChiamata = costoMinuto * minutiDurata;
         if (costoChiamata <= this.carica) {
             this.carica -= costoChiamata;
@@ -18,9 +18,15 @@ class FirstUser {
         }
     }
     numero404() {
+        //quì viene restituito il valore della carica disponibile.
+        return this.carica;
+    }
+    getNumeroChiamate() {
+        // restutuisce valore del numero delle chiamate effettuate.
         return this.numeroChiamate;
     }
     azzeraChiamate() {
+        //Quì azzzeriamo la variabile contenente il numero di chiamate effettuate.
         this.numeroChiamate = 0;
     }
 }
@@ -33,7 +39,7 @@ class SecondUser {
         this.carica += unaRicarica;
     }
     chiamata(minutiDurata) {
-        const costoMinuto = 0.20;
+        const costoMinuto = 0.2;
         const costoChiamata = costoMinuto * minutiDurata;
         if (costoChiamata <= this.carica) {
             this.carica -= costoChiamata;
@@ -44,6 +50,9 @@ class SecondUser {
         }
     }
     numero404() {
+        return this.carica;
+    }
+    getNumeroChiamate() {
         return this.numeroChiamate;
     }
     azzeraChiamate() {
@@ -59,17 +68,20 @@ class ThirdUser {
         this.carica += unaRicarica;
     }
     chiamata(minutiDurata) {
-        const costoMinuto = 0.20;
+        const costoMinuto = 0.2;
         const costoChiamata = costoMinuto * minutiDurata;
         if (costoChiamata <= this.carica) {
             this.carica -= costoChiamata;
             this.numeroChiamate++;
         }
         else {
-            console.log("Gentile cliente Wind, il suo saldo è insufficente per effettuare la chiamata.");
+            console.log("Gentile cliente Wind, il suo saldo è insufficiente per effettuare la chiamata.");
         }
     }
     numero404() {
+        return this.carica;
+    }
+    getNumeroChiamate() {
         return this.numeroChiamate;
     }
     azzeraChiamate() {
@@ -78,25 +90,25 @@ class ThirdUser {
 }
 // Utente Vodafone
 const utenteVodafone = new FirstUser();
-utenteVodafone.ricarica(15); // Qui sto ricaricando 10 euro
+utenteVodafone.ricarica(10); // Qui sto ricaricando 10 euro
 utenteVodafone.chiamata(10); //Effettuo una chiamata di 10 minuti (costo: 2 euro)
 utenteVodafone.chiamata(4); // Effettuo una seconda chiamata di 4 minuti (costo: 0.80 euro)
 utenteVodafone.chiamata(1); // Effettuo una terza chiamata di 1 minuti (costo: 0.20 euro)
-console.log("Gentile cliente Vodafone, il suo saldo disponibile è di euro:", utenteVodafone.carica);
-console.log(`Gentile cliente Vodafone, ha effettuato ${utenteVodafone.numero404()} chiamate.`);
+console.log(`Gentile cliente Vodafone, ha effettuato ${utenteVodafone.getNumeroChiamate()} chiamate.`);
+console.log("Gentile cliente Vodafone, il suo saldo disponibile è di euro:", utenteVodafone.numero404());
 //Utente Tim
 utenteVodafone.azzeraChiamate();
 const utenteTim = new SecondUser();
 utenteTim.ricarica(50); // Quì sto ricaricando 50 euro
 utenteTim.chiamata(30); // Effettuo una chiamata di 30 minuti (costo: 6 euro)
 utenteTim.chiamata(50); // Effettuo una seconda chiamata di 50 minuti (costo: 10 euro)
-console.log("Gentile cliente Tim, il suo saldo disponibile è di euro:", utenteTim.carica);
-console.log(`Gentile cliente Tim, ha effettuato ${utenteTim.numero404()} chiamate.`);
+console.log(`Gentile cliente Tim, ha effettuato ${utenteTim.getNumeroChiamate()} chiamate.`);
+console.log("Gentile cliente Tim, il suo saldo disponibile è di euro:", utenteTim.numero404());
 utenteTim.azzeraChiamate();
 //Utente Wind
 const utenteWind = new ThirdUser();
 utenteWind.ricarica(5); // Effettuo una ricarica da 5 euro
-utenteWind.chiamata(60); // Effettuo una chiamata di 60 minuti (12 euro) chiaramente il credito è insufficente.
-console.log("Gentile cliente Wind, il suo saldo disponibile è di euro:", utenteWind.carica);
-console.log(`Gentile cliente Wind, ha effettuato ${utenteWind.numero404()} chiamate.`);
+utenteWind.chiamata(60); // Effettuo una chiamata di 60 minuti (12 euro) chiaramente il credito è insufficiente.
+console.log(`Gentile cliente Wind, ha effettuato ${utenteWind.getNumeroChiamate()} chiamate.`);
+console.log("Gentile cliente Wind, il suo saldo disponibile è di euro:", utenteWind.numero404());
 utenteWind.azzeraChiamate();
